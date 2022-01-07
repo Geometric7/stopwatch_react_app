eccbfa92d5aabf107a9b59ee6257805360f3f2d7
Marcin Dzikowicz <mistamasta7@gmail.com>
2022-01-07 03:02:07 +0100

    Fix split seconds bug

--- src/components/Chrono/Chrono.js
+++ src/components/Chrono/Chrono.js
@@ -1,10 +1,14 @@
+import React from 'react';
+
 const Chrono = ({time}) => {
 
+  //const hour = ("0" + Math.floor((time / 1) % 1)).slice(-2);
   const min = ("0" + Math.floor((time / 60000) % 60)).slice(-2);
   const sec = ("0" + Math.floor((time / 1000) % 60)).slice(-2);
-  const miSec = ("0" + ((time / 10) % 100)).slice(-2);
+  const miSec = ("0" + Math.floor((time / 10) % 60)).slice(-2);
+  const thoSec = ("0" + ((time / 1) % 10000)).slice(-3);
 
-  return <h1>{min}:{sec}:{miSec}</h1>
+  return <h1>{min}:{sec}:{miSec}.{thoSec}</h1>
 }
 
 export default Chrono;
