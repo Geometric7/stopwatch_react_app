6ab725f7d452d78bb778b62ad5c9d65f26fe6eef
Marcin Dzikowicz <mistamasta7@gmail.com>
2022-01-08 14:39:46 +0100

    Fix seconds/mseconds display and add days

--- src/components/Chrono/Chrono.js
+++ src/components/Chrono/Chrono.js
@@ -3,12 +3,14 @@ import React from 'react';
 const Chrono = ({time}) => {
 
   //const hour = ("0" + Math.floor((time / 1) % 1)).slice(-2);
+
+  const day = ("0" + Math.floor((time / (3600000 * 24 * 365)) % 365 )).slice(-2);
+  const hour = ("0" + Math.floor((time / 3600000) % 24 )).slice(-2);
   const min = ("0" + Math.floor((time / 60000) % 60)).slice(-2);
   const sec = ("0" + Math.floor((time / 1000) % 60)).slice(-2);
-  const miSec = ("0" + Math.floor((time / 10) % 60)).slice(-2);
-  const thoSec = ("0" + ((time / 1) % 10000)).slice(-3);
+  const ms = ("0" + (time % 1000)).slice(-3);
 
-  return <h1>{min}:{sec}:{miSec}.{thoSec}</h1>
+  return <h1>{day}/{hour}:{min}:{sec}.{ms}</h1>
 }
 
 export default Chrono;
--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -10,31 +10,6 @@ const Timer = ({ currentTime, setCurrentTime, timer, setTimer }) => {
       }, 10));
       clearInterval(timer);
     }
-    /*
-  }
-  const msToTime = duration => {
-
-    // Pad 2|3 digits, def->2
-    const pad = (n, z) => {
-      z= z || 2;
-      return ('00' + n).slice(-z);
-    }
-
-    const ms = duration % 1000;
-    duration = (duration - ms) / 1000;
-    const secs = duration % 60;
-    duration = (duration - secs) / 60;
-    const mins = duration % 60;
-
-    return pad(mins) + ':' + pad(secs) + '.' + pad(ms,3);
-  }
-
-const start = () => {
-  setTimer(setInterval(() => {
-    setCurrentTime(currentTime => currentTime + 1);
-  }, 1 ));
-  clearInterval(timer);
-}*/
 
 const stop =() => {
   clearInterval(timer);
